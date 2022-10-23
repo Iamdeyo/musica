772cc875f760107001df9945a5c736581b865a8b
Ademola Taiwo <102713835+Iamdeyo@users.noreply.github.com>
2022-10-23 19:51:02 +0100

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="App text-red-500">Musica</div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      light: '#EFEEE0',
+      dark: '#1D2123',
+      secondary: '#FACD66',
+      primary: '#A4C7C6',
+      darkAlt: '#1A1E1F',
+      backG: '##1E1E1E',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
